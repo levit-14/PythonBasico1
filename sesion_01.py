7c4899a7ccfaf768cee287372cc3177c0f84f935
CBTis116 <levit-14>
2026-02-10 19:38:04 -0800

    agrege como usar condiciones en python

--- sesion_01.py
+++ sesion_01.py
@@ -12,3 +12,22 @@ print("===== Operadores matematicos =====")
 # /
 # ""
 # % MODULO
+print(int(2**3))
+print(int(4**8))
+print(float(10%2))
+print(float(25%4))
+
+ventas = 1999999
+print("Nuestras ventas fueron", ventas)
+
+is_active = True
+print(bool(is_active))
+
+game_over = False
+print(game_over)
+
+edad = 16
+if (edad >= 18):
+    print("Si puedo entrar al bar")
+else:
+    print("No puedo entrar al bar")
