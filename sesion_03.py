688f6b9c9bd419c70e0156e345391ddb3e5ea79f
levit-14 <agarcialev23@gmail.com>
2026-02-13 16:12:46 -0800

    Guardar mi practica 2

--- sesion_03.py
+++ sesion_03.py
@@ -11,3 +11,30 @@ print(f"El resultado de la suma de mi lista es: {resultado}")
 
 for i in range(2, 9):
     print(i)
+
+#while loop
+i=0
+
+while i<3:
+    i += 1
+    if i==3:
+        continue
+    print(i)
+    if i == 4:
+        break
+    else:
+        print("i es ahora mayor o igual a >")
+
+# Practica 2
+# Dada la lista mi_lista_2 = ("Lunes", "martes", "miercoles", "jueves", "viernes")
+# Imprimir cada elemento de la lista 3 veces y cuando sea lunes no lo incluyas
+# Pista: Usa los dos tipos loops while y for en el mismo programa para lograrlo
+
+i = 0
+mi_lista_2 = ["Lunes", "martes", "miercoles", "jueves", "viernes"]
+while i < 3:
+    i +=1
+
+for d in mi_lista_2:
+    if d != "lunes":
+        print(d)
