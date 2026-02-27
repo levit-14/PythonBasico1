9d1e8c0eceddae336216c50ed6ac7d1aad957316
levit-14 <agarcialev23@gmail.com>
2026-02-26 19:15:59 -0800

    guardar zombie

---
+++ OOP/zombie.py
@@ -0,0 +1,14 @@
+from enemigo import *
+
+
+class zombie(enemigo):
+    def __init__(self, puntos_energia=10, ataque=1):
+        super().__init__(tipo_enemigo='zombie', puntos_energia=puntos_energia, ataque=ataque)
+
+
+    def habla(self):
+        print("*Hummmm.....*")
+
+
+    def propagar_enfermedad(self):
+        print("El zombie esta tratando de propagar la enfermedad!!")
