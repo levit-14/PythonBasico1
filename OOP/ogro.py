526b82ccc4a3585a301f5b0e105fadced7cdfea9
levit-14 <agarcialev23@gmail.com>
2026-02-26 19:16:59 -0800

    guardar zombie

---
+++ OOP/ogro.py
@@ -0,0 +1,10 @@
+from enemigo import *
+
+
+class ogro(enemigo):
+    def __init__(self, puntos_energia=20, ataque=3):
+        super().__init__(tipo_enemigo='ogro', puntos_energia=puntos_energia, ataque=ataque)
+
+
+    def habla(self):
+        print("ogro aplasta todo!!!")
